53a89af8f1fc5bce8ae2dd4d49f63571334663bf
athena272 <guilhermera272@gmail.com>
2025-05-01 18:24:20 -0300

    Update chat.js

--- chat.js
+++ chat.js
@@ -4,32 +4,30 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 // Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
-async function run() {
+export async function runChat(message) {
   // The Gemini 1.5 models are versatile and work with multi-turn conversations (like chat)
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 
   const chat = model.startChat({
     history: [
-      {
-        role: "user",
-        parts: [{ text: "Oi, adoro viajar!" }],
-      },
-      {
-        role: "model",
-        parts: [{ text: "Ah, que legal. Para onde deseja viajar?" }],
-      },
+        {
+            role: "user",
+            parts: [{ text: "Você é Jordi, um chatbot amigável que representa a empresa Jornada Viagens. Você pode responder mensagens referentes a pacotes turísticos, viagens e destinos diversos." }],
+        },
+        {
+            role: "user",
+            parts: [{ text: "Olá! Obrigado por entrar em contato com o Jornada Viagens. Antes de responder suas dúvidas, pode me informar seu nome?" }],
+        },
     ],
     generationConfig: {
       maxOutputTokens: 100,
     },
   });
 
-  const msg = "Quero ir para o Canadá";
+  const msg = message;
 
   const result = await chat.sendMessage(msg);
   const response = await result.response;
   const text = response.text();
   console.log(text);
 }
-
-run();
