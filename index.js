c8caea22be376dc592f36f73c3e846425b04ea1a
athena272 <guilhermera272@gmail.com>
2025-05-01 16:18:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ import { makeQuestion } from './js/question.js';
 import { freeQuestion } from './js/freeQuestion.js';
 import { destinationQuery } from './js/destinationQuery.js';
 import { processesImage } from './js/processesImages.js';
+import { processesTextFile } from './js/categorizer.js';
 
 async function main() {
     const choice = await makeQuestion(`Escolha uma das opções abaixo: \n
-1. Fazer uma pergunta livre sobre um destino;
-2. Comparação de destinos por categorias;
-3. Ver informações com base em uma imagem;
-\nOpção desejada: `)
+    1. Fazer uma pergunta livre sobre um destino;
+    2. Comparação de destinos por categorias;
+    3. Ver informações com base em uma imagem;
+    4. Fazer a análise de sentimentos baseado em arquivo texto
+    \nOpção desejada: `);
 
     if (choice === '1') {
         await freeQuestion()
@@ -17,6 +19,8 @@ async function main() {
     } else if (choice === '3') {
         const image = await makeQuestion('\nMe informe o caminho completo e nome da imagem: ')
         await processesImage(image)
+    } else if (choice === '4') {
+        await processesTextFile()
     }
     else {
         console.log('Invalid choice')
