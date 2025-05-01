5ccc919973a5fd299b88749fbf0c9e016913cfb4
athena272 <guilhermera272@gmail.com>
2025-05-01 12:12:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,20 @@
-import 'dotenv/config';
-import { GoogleGenerativeAI } from '@google/generative-ai';
 import { makeQuestion } from './js/question.js';
-import { initializeModel } from './js/model.js';
+import { freeQuestion } from './js/freeQuestion.js';
+import { destinationQuery } from './js/destinationQuery.js';
 
-const model = initializeModel()
+async function main() {
+    const choice = await makeQuestion(`Escolha uma das opções abaixo: \n
+  1. Fazer uma pergunta livre sobre um destino;
+  2. Comparação de destinos por categorias;
+  \nOpção desejada: `)
 
-async function run() {
-    const categorias = await makeQuestion("Me fale as categorias que deseja visualizar sobre um determinado destino: ");
-    const prompt = await makeQuestion("Me fale sobre o destino que deseja conhecer: ");
-
-    const parts = [
-        { text: "Você é o chatbot de um site que vende pacotes de viagem. Ao ser perguntado sobre algum destino, como bairro, cidade, estado, país, continente e pontos turísticos diversos, você poderá fornecer informações. Caso seja perguntado sobre algo que não ter relação com viagem e turismo, informe que não poder responder a essa dúvida.\n\nPara formular a resposta, quero que os tópicos apareçam como lista com marcadores e sempre deve conter apenas  as categorias que forem solicitadas no momento da pergunta.\n\nAlguns exemplos de categorias: características, localização, cultura, pontos turísticos,  culinária, clima, dicas, como chegar, curiosidades." },
-        { text: "input: me fale sobre o bairro do flamengo, no Rio de Janeiro" },
-        { text: "output: **Bairro do Flamengo, Rio de Janeiro**\n\n* **Características:**\n    * Bairro nobre e residencial\n    * Conhecido por sua orla e parques\n    * Coração financeiro da cidade\n* **Localização:**\n    * Zona Sul do Rio de Janeiro\n    * Limita-se com as praias de Copacabana, Botafogo e Glória\n* **Cultura:**\n    * Sede do Museu de Arte Moderna (MAM) e da Marina da Glória\n    * Abriga o Parque do Flamengo, um dos maiores parques urbanos do país\n* **Pontos Turísticos:**\n    * Orla do Flamengo (anel viário com ciclovia e vista panorâmica)\n    * Museu de Arte Moderna (MAM)\n    * Marina da Glória (e palco do Reveillon)\n    * Parque do Flamengo\n    * Morro da Viúva (com vista para o Pão de Açúcar)\n* **Culinária:**\n    * Diversos restaurantes e bares na orla e no Parque do Flamengo\n    * Opções gastronômicas variadas, de frutos do mar a comida internacional" },
-        { text: `input: me fale sobre ${categorias} o destino ${prompt}` },
-        { text: "output: " },
-    ];
-
-    const result = await model.generateContent(
-        { contents: [{ role: "user", parts }] }
-    );
-    const response = await result.response;
-    const text = response.text();
-    console.log(text);
+    if (choice === '1') {
+        await freeQuestion()
+    } else if (choice === '2') {
+        await destinationQuery()
+    } else {
+        console.log('Invalid choice')
+    }
 }
 
-run();
+main()
