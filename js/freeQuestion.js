98f302dded0311d7a50faf5e67eea5cace939337
athena272 <guilhermera272@gmail.com>
2025-05-01 12:06:24 -0300

    Create freeQuestion.js

---
+++ js/freeQuestion.js
@@ -0,0 +1,23 @@
+import 'dotenv/config';
+import { GoogleGenerativeAI } from '@google/generative-ai';
+import { makeQuestion } from './question.js';
+import { initializeModel } from './model.js';
+
+const model = initializeModel('gemini-exp-1206')
+
+export async function freeQuestion() {
+    const prompt = await fazerPergunta("Me faça uma pergunta sobre um determinado destino: ");
+
+    const parts = [
+        {text: "Você é o chatbot de um site que vende pacotes de viagem." },
+        {text: `input: ${prompt}`},
+        {text: "output: "},
+      ];
+
+    const result = await model.generateContent(
+        { contents: [{ role: "user", parts }] }
+    );
+    const response = await result.response;
+    const text = response.text();
+    console.log(text);
+}
