3d4493425c9d35adf9f8405c9c4db6ffa09846f9
athena272 <guilhermera272@gmail.com>
2025-05-01 21:39:11 -0300

    Update chat.js

--- chat.js
+++ chat.js
@@ -4,26 +4,26 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 // Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
-export async function runChat(message) {
-  // The Gemini 1.5 models are versatile and work with multi-turn conversations (like chat)
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+// The Gemini 1.5 models are versatile and work with multi-turn conversations (like chat)
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-  const chat = model.startChat({
-    history: [
-        {
-            role: "user",
-            parts: [{ text: "Você é Jordi, um chatbot amigável que representa a empresa Jornada Viagens. Você pode responder mensagens referentes a pacotes turísticos, viagens e destinos diversos." }],
-        },
-        {
-            role: "model",
-            parts: [{ text: "Olá! Obrigado por entrar em contato com o Jornada Viagens. Antes de responder suas dúvidas, pode me informar seu nome?" }],
-        },
-    ],
-    generationConfig: {
-      maxOutputTokens: 1000,
+const chat = model.startChat({
+  history: [
+    {
+      role: "user",
+      parts: [{ text: "Você é Jordi, um chatbot amigável que representa a empresa Jornada Viagens. Você pode responder mensagens referentes a pacotes turísticos, viagens e destinos diversos." }],
     },
-  });
+    {
+      role: "model",
+      parts: [{ text: "Olá! Obrigado por entrar em contato com o Jornada Viagens. Antes de responder suas dúvidas, pode me informar seu nome?" }],
+    },
+  ],
+  generationConfig: {
+    maxOutputTokens: 1000,
+  },
+});
 
+export async function runChat(message) {
   const result = await chat.sendMessage(message);
   const response = await result.response;
   return response.text();
