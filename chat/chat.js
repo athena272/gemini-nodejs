fc0c82b101cd91f2129ad69f5fcbe84a7d7b473b
athena272 <guilhermera272@gmail.com>
2025-05-02 08:49:37 -0300

    Update chat.js

--- chat/chat.js
+++ chat/chat.js
@@ -1,6 +1,7 @@
 import { chat } from './initializeChat.js'
 
 export async function runChat(message) {
+  console.log("🚀 ~ runChat ~ (await chat.getHistory()).length:", (await chat.getHistory()).length)
   const result = await chat.sendMessage(message);
   const response = await result.response;
   return response.text();
