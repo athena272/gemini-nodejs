72bc6899a158318b11bbd166f53a68dace92a5d6
athena272 <guilhermera272@gmail.com>
2025-05-02 09:08:39 -0300

    Update chat.js

--- chat/chat.js
+++ chat/chat.js
@@ -4,5 +4,37 @@ export async function runChat(message) {
   console.log("🚀 ~ runChat ~ (await chat.getHistory()).length:", (await chat.getHistory()).length)
   const result = await chat.sendMessage(message);
   const response = await result.response;
-  return response.text();
+
+  const content = response.candidates[0].content;
+ 
+  const fc = content.parts[0].functionCall;
+  const text = content.parts.map(({ text }) => text).join("");
+  console.log(text);
+  console.log(fc);
+   
+  if (fc) {
+    const { name, args } = fc;
+    const fn = funcoes[name];
+    if (!fn) {
+      throw new Error(`Unknown function "${name}"`);
+    }
+    const fr = {
+      functionResponse: {
+          name,
+          response: {
+          name,
+           content: funcoes[name](args),
+          }
+      },
+    }
+  
+    console.log(fr)
+  
+    const request2 = [fr];
+    const response2 = await chat.sendMessage(request2);
+    const result2 = response2.response;
+    return result2.text();
+  } else if (text) {
+    return text;
+  }
 }
