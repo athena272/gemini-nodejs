8c9fc9b3adca048f5771017332b5e88ffc8b39ee
athena272 <guilhermera272@gmail.com>
2025-05-02 08:49:34 -0300

    Update initializeChat.js

--- chat/initializeChat.js
+++ chat/initializeChat.js
@@ -9,6 +9,19 @@ const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 let chat
 
+const functions = {
+    installmentInterestRate: ({ value }) => {
+        const months = typeof value === "string" ? parseInt(value) : value;
+        if (months <= 6) {
+            return 3;
+        } else if (months <= 12) {
+            return 5;
+        } else if (months <= 24) {
+            return 7;
+        }
+    }
+};
+
 function initializeChat() {
     chat = model.startChat({
         history: [
